df75082a3a0d8f6fe287605ea45f3c853f9023c8
tug48427 <tug48427@temple.edu>
2019-09-25 23:13:44 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/coloractivity/MainActivity.java
@@ -0,0 +1,48 @@
+package edu.temple.coloractivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.Constraints;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.provider.SyncStateContract;
+import android.text.Layout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ConstraintLayout mylayout=findViewById(R.id.layout);
+
+        final Spinner textSpinner = findViewById(R.id.spinner);
+        final String colors[] =  {"Blue","Cyan", "Gray","Green","Magenta", "Red","Yellow", "Fuchsia", "DarkGray","LightGray" };
+
+        final ColorAdapter adapter = new ColorAdapter(this,colors,colors.length);
+
+        textSpinner.setAdapter(adapter);
+
+        textSpinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l){
+                view.setBackgroundColor(Color.WHITE);
+
+                mylayout.setBackgroundColor(Color.parseColor(colors[i]));
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+}
